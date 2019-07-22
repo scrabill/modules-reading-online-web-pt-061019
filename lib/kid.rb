022b05f019604e994d217f57af9977a9fd5b57d3
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-22 02:23:40 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,7 @@ require_relative './class_methods_module.rb'
 
 class Kid
 
-  
+
   extend MetaDancing
 
   include Dance
@@ -12,5 +12,5 @@ class Kid
 
   def initialize(name)
     @name = name
-  end  
+  end
 end
